45cd3a72a3fba7a2d553b7866849235c55fd1787
Ade Priyanto <ade.priyanto06@gmail.com>
2019-05-23 15:00:43 +0700

    update api rule

--- src/main/java/com/phincon/spice/rest/RuleController.java
+++ src/main/java/com/phincon/spice/rest/RuleController.java
@@ -1,9 +1,13 @@
 package com.phincon.spice.rest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,9 +23,32 @@ public class RuleController {
 	@Autowired
 	RuleService ruleService;
 	
-	@RequestMapping(value = "/get_rule", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/company_rule", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> companyRule(@RequestBody RestHub restHub) throws Exception{
+		Map<String, Object> message = restHub.getMessage();
+		String pesan = "company_rule";
+		
+		if(message == null) {
+			message = new HashMap<>();
+		}
+		message.put("pesan", pesan);
+		restHub.setMessage(message);
+		
+		Object out = ruleService.getRule(restHub);
+		return new ResponseEntity<Object>(out, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/medical_claim_rule", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> loginUser(@RequestBody RestHub restHub) throws Exception{
-		System.out.println("MESSAGE REQ "+restHub.getMessage());
+		Map<String, Object> message = restHub.getMessage();
+		String pesan = "claim_medical_rule";
+		
+		if(message == null) {
+			message = new HashMap<>();
+		}
+		message.put("pesan", pesan);
+		restHub.setMessage(message);
+		
 		Object out = ruleService.getRule(restHub);
 		return new ResponseEntity<Object>(out, HttpStatus.OK);
 	}
